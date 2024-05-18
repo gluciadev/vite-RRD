48bea5a025caca77908cc7d2dfbb6dea775ea8ec
GuillermoL <guillermo.lucia.dev@gmail.com>
2024-05-19 00:36:35 +0200

    feat: added some query params examples

--- vite-RRD-rutas-dinamicas/src/screens/About.tsx
+++ vite-RRD-rutas-dinamicas/src/screens/About.tsx
@@ -2,10 +2,19 @@ import { useNavigate, useParams } from 'react-router-dom';
 
 export const About = () => {
     const navigate = useNavigate();
+
+    //URL params
     const { name , age, lastname } = useParams();
+
+    //Query params
+    const queryParams = new URLSearchParams(location.search);
+    const petName = queryParams.get("petName");
+    const color = queryParams.get("color");
+
     return (
       <div>
           <h1>About {name} {lastname} {age}</h1>
+          <h2>{petName} {color}</h2>
           <button onClick={()=>navigate("/")}>Home Screen</button>
       </div>
     )
--- vite-RRD-rutas-dinamicas/src/screens/Home.tsx
+++ vite-RRD-rutas-dinamicas/src/screens/Home.tsx
@@ -7,8 +7,9 @@ export const Home = () => {
         <h1>Home</h1>
         <button onClick={()=>navigate("/about/Juan/12/Lucas")}>About Screen with all params</button>
         <br></br>
-        <br></br>
         <button onClick={()=>navigate("/about/Juan/Martín")}>About Screen without all params</button>
+        <br></br>
+        <button onClick={()=>navigate("/about/Juan/Martín?petName=Acrux&color=blanco")}>About Screen with query params</button>
     </div>
   )
 }
